2d8faf4a8bfdb0a105585d111dae3fc407aeb423
nikhildighe <nikhildighe2011@gmail.com>
2023-08-23 04:10:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Temp from './component/temp';
+
+const App = () => {
+  return (
+    <div>
+      <Temp/>
+    </div>
+  )
+}
+                                                                       
+export default App
